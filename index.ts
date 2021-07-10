a4c7e7788e6356ef82615c3363721d6b8a206d20
plylrnsdy <plylrnsdy@163.com>
2021-07-10 14:45:17 +0800

    support custom swc configuration
    - custom configuration in `jest.config.js`
    - load `.swcrc` in default

--- index.ts
+++ index.ts
@@ -1,27 +1,49 @@
-import { transformSync } from '@swc/core'
+import * as fs from 'fs'
+import * as path from 'path'
+import { transformSync, Options } from '@swc/core'
+
+
+let transformOpts: Options
 
 export = {
-  process(src: string, path: string, jestConfig: any) {
-    const [, , transformOptions = {}] =
-      (jestConfig.transform || []).find(([, transformerPath]: [string, string]) => transformerPath === __filename) || []
+  process(src: string, filepath: string, jestConfig: any) {
 
-    if (/\.(t|j)sx?$/.test(path)) {
-      return transformSync(src, {
-        ...transformOptions,
-        filename: path,
-        jsc: {
-          transform: {
-            //@ts-ignore
-            hidden: {
-              jest: true
-            }
-          },
-        },
-        module: {
-          type: "commonjs"
+    if (/\.(t|j)sx?$/.test(filepath)) {
+
+      if (!transformOpts) {
+        const isSwcJestTransformer = ([, transformerPath]: [unknown, string]) => transformerPath === __filename
+
+        let [, , swcOptions] = (jestConfig.transform || []).find(isSwcJestTransformer) || []
+
+        if (!swcOptions) {
+          const swcrc = path.join(process.cwd(), '.swcrc')
+          swcOptions = fs.existsSync(swcrc) ? JSON.parse(fs.readFileSync(swcrc, 'utf-8')) : {}
         }
-      })
+
+        set(swcOptions, 'module.type', 'commonjs')
+        set(swcOptions, 'jsc.transform.hidden.jest', true)
+
+        transformOpts = swcOptions
+      }
+
+      transformOpts.filename = filepath
+
+      return transformSync(src, transformOpts)
     }
+
     return src
   },
+}
+
+function set(obj: any, path: string, value: any) {
+  let o = obj
+  const parents = path.split('.')
+  const key = parents.pop() as string
+
+  for (const prop of parents) {
+    if (o[prop] == null) o[prop] = {}
+    o = o[prop]
+  }
+
+  o[key] = value
 }
