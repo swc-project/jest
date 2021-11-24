4748afd23d6d4a38c4e85d4a5963155825d158d2
Keisuke Kan <9renpoto@gmail.com>
2021-11-24 17:11:13 +0900

    chore: upgrade react

--- examples/react/src/App.test.tsx
+++ examples/react/src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { App } from "./App";
 import { create } from 'react-test-renderer'
 
--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function App() {
   return <div>hello</div>;
 }
